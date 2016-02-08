f392c6783a9609ddb5eeb238730e537fc8c0d93b
unknown <CSHORTE@DIS-CSHORTE-1.ad.nsf.gov>
2016-02-08 16:09:35 -0500

    Updated select-type-funding component to update wizard cache responses when user selects an option.

--- app/components/wizard/select-type-funding/component.js
+++ app/components/wizard/select-type-funding/component.js
@@ -5,6 +5,14 @@ export default Ember.Component.extend({
   actions: {
     previous: function() {
       this.sendAction('previous');
+    },
+    setType: function(type) {
+      let wizard = this.get('wizard');
+      wizard.set('type', type);
+    },
+    setFundingMech: function(fundingMech) {
+      let wizard = this.get('wizard');
+      wizard.set('fundingMech', fundingMech);
     }
   }
 
--- app/wizard/model.js
+++ app/wizard/model.js
@@ -4,6 +4,9 @@ export default DS.Model.extend({
 
   fundingOp: DS.attr('wizard/funding-opportunity'),
 
+  type: DS.attr('string'),
+  fundingMech: DS.attr('string')
+
   //has many or an array of [Division, Program] tuples
 
 });
