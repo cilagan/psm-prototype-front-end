b2f32837ad707b57cf13eac3571819295f13a307
unknown <CSHORTE@DIS-CSHORTE-1.ad.nsf.gov>
2016-02-08 14:46:41 -0500

    Added a funding opportunity field to the wizard cache object

--- app/wizard/model.js
+++ app/wizard/model.js
@@ -2,6 +2,8 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
 
-  
+  fundingOp: DS.attr('wizard/funding-opportunity'),
+
+  //has many or an array of [Division, Program] tuples
 
 });
