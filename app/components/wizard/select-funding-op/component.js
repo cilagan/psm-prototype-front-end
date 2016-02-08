8112f507dd6b8d1302de0536bb5203060783fd70
unknown <CSHORTE@DIS-CSHORTE-1.ad.nsf.gov>
2016-02-08 14:45:38 -0500

    Added dependencies: ember-truth-helpers (HTMLBars template helpers for additional truth logic in if and unless statements https://www.npmjs.com/package/ember-truth-helpers), ember-radio-button (radio buttons are not a part of core ember, https://github.com/yapplabs/ember-radio-button)
    Updated environment.js development variables for more verbose development logging.
    
    Updated select-funding-op component to successfully use radio buttons, save selection to cache, and display selection upon return to this wizard screen. Victory!!

--- app/components/wizard/select-funding-op/component.js
+++ app/components/wizard/select-funding-op/component.js
@@ -2,10 +2,14 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
 
+
   actions: {
     next: function() {
       this.sendAction('next');
+    },
+    setFundingOp: function(fundingOp) {
+      let wizard = this.get('wizard');
+      wizard.set('fundingOp', fundingOp);
     }
   }
-
 });
