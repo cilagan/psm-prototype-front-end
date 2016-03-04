3450457753702cf437a38e0a21f7933db65dfce9
Chelsea Shorte <cshorte@associates.nsf.gov>
2016-03-04 14:15:53 -0500

    Updates for search icon

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -17,6 +17,23 @@ module.exports = function(defaults) {
   app.import('bower_components/jquery/dist/jquery.min.js');
   app.import('bower_components/bootstrap-sass/assets/javascripts/bootstrap.min.js');
 
+  /*Bootstrap Fonts*/
+  app.import('bower_components/bootstrap-sass/assets/fonts/bootstrap/glyphicons-halflings-regular.eot', {
+    destDir: 'fonts/bootstrap'
+  });
+  app.import('bower_components/bootstrap-sass/assets/fonts/bootstrap/glyphicons-halflings-regular.svg', {
+    destDir: 'fonts/bootstrap'
+  });
+  app.import('bower_components/bootstrap-sass/assets/fonts/bootstrap/glyphicons-halflings-regular.ttf', {
+    destDir: 'fonts/bootstrap'
+  });
+  app.import('bower_components/bootstrap-sass/assets/fonts/bootstrap/glyphicons-halflings-regular.woff', {
+    destDir: 'fonts/bootstrap'
+  });
+  app.import('bower_components/bootstrap-sass/assets/fonts/bootstrap/glyphicons-halflings-regular.woff2', {
+    destDir: 'fonts/bootstrap'
+  });
+
   /*Styles*/
 
   //Need bower packages bootstrap-fileinput
