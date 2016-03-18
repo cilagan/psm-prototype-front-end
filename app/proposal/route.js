934c4d9ca6f28bed29e3a6cc83bfd6c6834ad340
unknown <CSHORTE@DIS-CSHORTE-1.ad.nsf.gov>
2016-03-18 16:05:33 -0400

    Line space changes.

--- app/proposal/route.js
+++ app/proposal/route.js
@@ -4,9 +4,13 @@ import $ from 'jquery';
 export default Ember.Route.extend({
   model(params) {
     // return this.store.findRecord('proposal', params.proposal_id);
+
     return $.getJSON('/docService/proposal/'+params.proposal_id+'/metadata').then(function(json) {
       return json.proposal;
     });
+
+
+
   }/*,
   proposal: Ember.computed.alias('model')*/
 });
