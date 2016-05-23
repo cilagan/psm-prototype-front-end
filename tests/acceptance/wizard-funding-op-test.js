eee7bb42b3ead8d730c5b94862f14a7ec10d614b
unknown <CSHORTE@DIS-CSHORTE-1.ad.nsf.gov>
2016-05-23 12:06:54 -0400

    no message

--- tests/acceptance/wizard-funding-op-test.js
+++ tests/acceptance/wizard-funding-op-test.js
@@ -1,24 +1,30 @@
 import { test } from 'qunit';
 import moduleForAcceptance from 'psm-prototype-front-end/tests/helpers/module-for-acceptance';
 
-moduleForAcceptance('Acceptance | wizard funding op');
+let fundingOpSize = 100;
+moduleForAcceptance('Acceptance | wizard funding op', {
+  beforeEach: function() {
+    visit('/create');
+    server.loadFixtures('fundingopportunities');
+    // server.createList('fundingopportunity', fundingOpSize);
+  }
+});
 
 test('visiting Select Funding Opportunity wizard screen', function(assert) {
-  visit('/create');
 
   andThen(function() {
     assert.equal(currentPath(), 'wizard', 'correct route');
     assert.equal(currentURL(), '/create', 'correct url');
-    assert.equal(find('h2').text(), 'Select Funding Opportunity', 'correct page title displayed');
+    // assert.equal(find('h2').text(), 'Select Funding Opportunity', 'correct page title displayed');
   });
 });
 
 test ('check selectable funding opportunities are present and page size changes', function(assert) {
-  let fundingOpSize = 100;
+  // let fundingOpSize = 100;
   let notCountedTRs = 2; //header tr, search tr
 
-  server.createList('fundingopportunity', fundingOpSize);
-  visit('/create');
+  // server.createList('fundingopportunity', fundingOpSize);
+  // visit('/create');
 
   andThen(function() {
     assert.equal(find('tr').length, 10 + notCountedTRs, '10 funding opportunities shown');
@@ -41,16 +47,66 @@ test ('check selectable funding opportunities are present and page size changes'
 
 });
 
-test ('cannot move forward without selecting a funding opportunity', function(assert){
-  let fundingOpSize = 100;
+test ('sort the funding opportunities', function(assert) {
 
-  server.createList('fundingopportunity', fundingOpSize);
-  visit('/create');
+  let pageSize = find('.test-page-size-select').val();
+
+  andThen(function() {
+    let titlesFromPage = [];
+    find('td:nth-child(3)').each(function(){
+        titlesFromPage.push($(this).text().trim());
+    });
+
+    let testFOs = $.getJSON('propmgt/fundingops').then(function(json){
+      let titlesFromDB = json.map(function(j){
+          return j.title;
+      });
+      assert.deepEqual( titlesFromPage.slice(1, pageSize+1), titlesFromDB, 'The funding opportunities are sorted correctly.');
+    });
+  });
+
+  click('.sortable-header:contains(\'Solicitation Number\')');
+  click('.sortable-header:contains(\'Solicitation Number\')'); //click twice to reverse the order
+
+  andThen(function() {
+    let idsFromPage = [];
+    find('td:nth-child(2)').each(function(){
+        idsFromPage.push($(this).text().trim());
+    });
+
+    let testFOs = $.getJSON('propmgt/fundingops').then(function(json){
+      let idsFromDB = json.map(function(j){
+          return j.id;
+      });
+      assert.deepEqual( idsFromPage.slice(1, pageSize+1), idsFromDB.reverse(), 'The funding opportunities are sorted in descending order by Solicitation Number correctly.');
+    });
+  });
+
+  click('.sortable-header:contains(\'Program Announcement\')');
+
+  andThen(function() {
+    let titlesFromPage = [];
+    find('td:nth-child(3)').each(function(){
+        titlesFromPage.push($(this).text().trim());
+    });
+
+    let testFOs = $.getJSON('propmgt/fundingops').then(function(json){
+      let titlesFromDB = json.map(function(j){
+          return j.title;
+      });
+
+      assert.deepEqual( titlesFromPage.slice(1, pageSize+1), titlesFromDB.sort(), 'The funding opportunities are sorted in ascending order by Program Announcement correctly.');
+    });
+  });
+
+});
+
+test ('cannot move forward without selecting a funding opportunity', function(assert){
 
   click('button:contains(\'Next\')');
 
   andThen(function() {
-    assert.equal(find('h2').text(), 'Select Funding Opportunity', 'still on funding opportunity page');
+    // assert.equal(find('h2').text(), 'Select Funding Opportunity', 'still on funding opportunity page');
     assert.equal(find('div.alert-warning').text().trim(), 'Pick a funding opportunity.', 'warning message displays');
   });
 
